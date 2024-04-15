f9850256b0ae15ad0c02095d15c1fe9a7ac204da
Viktor Kolev <viktorkolev0707@gmail.com>
2024-04-15 21:57:15 +0300

    Fix background image
    Background image is now dynamic - will change on window resize. Desktop / Mobile background is enabled depending on window size upon load.

--- assets/JS-scripts/scripts.js
+++ assets/JS-scripts/scripts.js
@@ -1,21 +1,41 @@
-// All needed elements are selected, then function listens for a click on the formBtn. When the formBtn is clicked, the if statement is initiated, which adds/removes the invalid and open-inline classes to certain elements. These classes apply the needed styles for the user to see if they have entered a valid email address and if not - why it is not valid. If the entered email is valid, the function removes the open class from all elements and adds it to the successForm element. The user is then able to click on the dismissBtn for the
-
 (function inputForm() {
+  // All needed elements are selected-
   const formCont = document.querySelector(".form-container");
   const ctaForm = document.querySelector(".form-cta");
   const successForm = document.querySelector(".form-success");
   const backgrImg = document.querySelector(".background-img");
+  const mobileBackgrImg = document.querySelector(".mobile-background-img");
   const dismissBtn = document.querySelector(".dismiss");
   const noEmailErr = document.querySelector(".no-email");
   const invalidEmailErr = document.querySelector(".invalid-email");
   const inputEmail = document.querySelector("#email");
   const formBtn = document.querySelector(".cta-btn");
 
-  ctaForm.noValidate = true;
+  window.addEventListener("load", () => {
+    if (window.innerWidth <= 600) {
+      mobileBackgrImg.classList.add("open");
+    } else {
+      backgrImg.classList.add("open");
+    }
+  });
 
+  window
+    .matchMedia("screen and (min-width: 600px)")
+    .addEventListener("change", ({ matches }) => {
+      if (matches) {
+        backgrImg.classList.add("open");
+        mobileBackgrImg.classList.remove("open");
+      } else {
+        backgrImg.classList.remove("open");
+        mobileBackgrImg.classList.add("open");
+      }
+    });
+
+  ctaForm.noValidate = true;
+  // -,then function listens for a click on the formBtn.
   formBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
+    // When the formBtn is clicked, the if statement is initiated, which adds/removes the invalid and open-inline classes to certain elements. These classes apply the needed styles for the user to see if they have entered a valid email address and if not - why it is not valid.
     if (inputEmail.value === "") {
       ctaForm.classList.add("invalid");
       noEmailErr.classList.add("open-inline");
@@ -25,21 +45,27 @@
       noEmailErr.classList.remove("open-inline");
       invalidEmailErr.classList.add("open-inline");
     } else {
+      // If the entered email is valid, the function removes the open class from all elements and adds it to the successForm element.
       formCont.classList.remove("open");
       backgrImg.classList.remove("open");
+      mobileBackgrImg.classList.remove("open");
       noEmailErr.classList.remove("open-inline");
       successForm.classList.add("open");
       let successEmail = document.querySelector(".success-email");
       successEmail.textContent = `${inputEmail.value}`;
     }
   });
-
+  // The user is then able to click on the dismissBtn in order to return to the original CTA page.
   dismissBtn.addEventListener("click", () => {
     successForm.classList.remove("open");
     formCont.classList.add("open");
-    backgrImg.classList.add("open");
     ctaForm.classList.remove("invalid");
     invalidEmailErr.classList.remove("open-inline");
+    if (window.innerWidth > 600) {
+      backgrImg.classList.add("open");
+    } else {
+      mobileBackgrImg.classList.add("open");
+    }
     ctaForm.reset();
   });
 })();
